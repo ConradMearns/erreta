4f4eec0c319efcf25c29c3908b202a71fbede62d
Conrad Mearns <conradmearns@gmail.com>
2019-11-03 10:09:13 -0800

    Remove webpack

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 use wasm_bindgen::prelude::*;
 
-// pub fn run() {
-//     bare_bones();
-//     using_a_macro();
-//     using_web_sys();
-// }
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global 'window' exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have body");
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
 }
